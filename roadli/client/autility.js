e4aab42fceeed04b54611110146bfa6ff454e771
Doug Feigelson <dougfeig@gmail.com>
2013-09-12 17:28:17 -0400

    mobile support

---
+++ roadli/client/autility.js
@@ -0,0 +1,189 @@
+GAQ_ACCOUNT = 'UA-42915189-1'
+// http://stackoverflow.com/questions/7865446/google-maps-places-api-v3-autocomplete-select-first-option-on-enter
+
+// setup Google Analytics
+// To log an event, e.g.:
+// window._gaq.push(['_trackEvent','FAQ','Click',Session.get('server_id')]);
+Template.main.created = function() {
+  if (!window._gaq) {
+    window._gaq = [];
+    var pluginUrl = 
+     '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+    _gaq.push(['_setAccount', GAQ_ACCOUNT]);  
+    if (!Session.get('pageview_tracked')) {
+      _gaq.push(['_trackPageview']);      
+    }
+    Session.set('pageview_tracked',true); // prevent hot code push from tracking another pageview
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  }
+};
+
+hideAndroidKeyboard = function(element) {
+    element.attr('readonly', 'readonly'); // Force keyboard to hide on input field.
+    element.attr('disabled', 'true'); // Force keyboard to hide on textarea field.
+    setTimeout(function() {
+        element.blur();  //actually close the keyboard
+        // Remove readonly attribute after keyboard is hidden.
+        element.removeAttr('readonly');
+        element.removeAttr('disabled');
+    }, 100);
+}
+
+
+autoSelectOnTab = function(input) {
+    (function pacSelectFirst(input) {
+        // store the original event binding function
+        var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+
+        function addEventListenerWrapper(type, listener) {
+            // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
+            // and then trigger the original listener.
+            if (type == "keydown") {
+                var orig_listener = listener;
+                listener = function(event) {
+                    var suggestion_selected = $(".pac-item.pac-selected").length > 0;
+                    
+                    // enter or tab
+                    if ((event.which == 9 || event.which == 13) && !suggestion_selected) {
+                        var simulated_downarrow = $.Event("keydown", {
+                            keyCode: 40,
+                            which: 40
+                        });
+                        orig_listener.apply(input, [simulated_downarrow]);
+                    }
+                    orig_listener.apply(input, [event]);
+                };
+            }
+            _addEventListener.apply(input, [type, listener]);
+        }
+        input.addEventListener = addEventListenerWrapper;
+        input.attachEvent = addEventListenerWrapper;
+        // var autocomplete = new google.maps.places.Autocomplete(input);
+    })(input);
+};
+
+ secsToStr = function(secs) {
+    if (secs === null) {
+        return ''
+    }
+
+    if (secs<0) {
+        return 'no additional time';
+    }
+
+    function numberEnding (number) { //todo: replace with a wiser code
+        return (number > 1) ? 's' : '';
+    }
+    var years = Math.floor(secs / 31536000);
+    if (years) {
+        return years + ' year' + numberEnding(years);
+    }
+    var days = Math.floor((secs %= 31536000) / 86400);
+    if (days) {
+        return days + ' day' + numberEnding(days);
+    }
+    var hours = Math.floor((secs %= 86400) / 3600);
+    if (hours) {
+        return hours + ' hr' + numberEnding(hours);
+    }
+    var minutes = Math.floor((secs %= 3600) / 60);
+    if (minutes) {
+        return minutes + ' min' + numberEnding(minutes);
+    }
+    var seconds = secs % 60;
+    if (seconds) {
+        return seconds + ' sec' + numberEnding(seconds);
+    }
+    return 'no additional time'; //'just now' //or other string you like;
+}
+
+getParameterByName = function(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+updateQueryStringParameter = function(key, value) {
+  if (!value) value='';
+  var uri = document.location.href;
+  var re = new RegExp("([?|&])" + key + "=.*?(&|$)", "i");
+  separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  var res;
+  if (uri.match(re)) {
+    res = uri.replace(re, '$1' + key + "=" + value + '$2');
+  }
+  else {
+    res = uri + separator + key + "=" + value;
+  }
+  window.history.pushState({},"", res);
+}
+
+
+// Draw the array of boxes as polylines on the map
+drawBoxes = function(boxes) {
+  var boxpolys = new Array(boxes.length);
+  for (var i = 0; i < boxes.length; i++) {
+    boxpolys[i] = new google.maps.Rectangle({
+      bounds: boxes[i],
+      fillOpacity: 0,
+      strokeOpacity: 1.0,
+      strokeColor: '#000000',
+      strokeWeight: 1,
+      map: map
+    });
+  }
+};
+
+
+$.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop) -scrollPane.offset().top;
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
+
+$.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+};
